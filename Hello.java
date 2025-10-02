759dbd254c2ee3c7ac260baf842856ec98e02f6e
“gadealba” <gadealba@umich.edu>
2025-10-02 18:07:55 -0400

    added correct code

--- Hello.java
+++ Hello.java
@@ -1,6 +1,12 @@
-Public class HelloWorld{
-    public static void main(String[] args){
-            System.out.println("hello World!");
-        }
+public class Hello {
+	public static void main(String[] args) {
+
+		for(int i=1;i<=10;i++) {
+
+			System.out.println("Hello World..."+i);
+		}
+	}
 }
+
+
     
